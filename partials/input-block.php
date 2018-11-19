7feb732c59ad9a48df4cb8b2c9e8c07a11d6afbd
DEMENTOR <dmntr@rambler.ru>
2018-11-19 23:30:28 +1000

    Update input-block.php

--- partials/input-block.php
+++ partials/input-block.php
@@ -1,4 +1,4 @@
-<div class="revealator-zoomin revealator-duration10 revealator-partially-above revealator-once" id="chat-block">
+<div class="revealator-zoomin revealator-duration10 revealator-position-within revealator-once" id="chat-block">
   <!-- Поле ввода текста -->
   <input autofocus autocomplete="off" placeholder="Наберите текст" id="message-input"/>
   <!-- Кнопка отправки сообщения -->
